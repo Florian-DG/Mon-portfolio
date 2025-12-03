aa05b91982e3d3e594a52376ea8c7f02d0958b7f
Florian-DG <f.degeiter@gmail.com>
2025-12-03 10:41:18 +0100

    page projets

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,3 @@ const mobileMenu = document.querySelector('#mobileMenu');
 hamburger.addEventListener("click", () => {
     mobileMenu.classList.toggle('show');
 });
-
-const track = document.querySelector('.carousel-track');
-const items = document.querySelectorAll('.carousel-item');
-
-let index = 0;
-
-function updateCarousel() {
-    track.style.transform = `translateX(-${index * 100}%)`;
-}
-
-document.querySelector('.next').addEventListener('click', () => {
-    index = (index + 1) % items.length;
-    updateCarousel();
-});
-
-document.querySelector('.prev').addEventListener('click', () => {
-    index = (index - 1 + items.length) % items.length;
-    updateCarousel();
-});
