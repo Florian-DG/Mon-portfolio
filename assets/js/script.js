18442c2e8b4fd69aeb6dd8cbced5a56e227ab83f
Florian-DG <f.degeiter@gmail.com>
2025-12-15 09:12:04 +0100

    Page brasserie et correction de sectionnement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 const hamburger = document.querySelector('#hamburger');
 const mobileMenu = document.querySelector('#mobileMenu');
 
+
 hamburger.addEventListener("click", () => {
     mobileMenu.classList.toggle('show');
+});
+
+
+document.querySelectorAll(".projet").forEach(element => {
+  element.addEventListener("click", (e) => {
+    window.location.assign(e.currentTarget.dataset.href)
+  });
 });
